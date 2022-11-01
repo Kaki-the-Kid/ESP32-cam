8b107c92433727fbcde4c8cdd5db463892b87c60
Kaki-the-Kid <karsten.reitan@gmail.com>
2022-11-01 12:20:43 +0100

    Init commit

--- image2cloud/image2cloud.ino
+++ image2cloud/image2cloud.ino
@@ -1,5 +1,16 @@
 
 
-// We don´t put any component on the module at first
-const int default_temp = 19;
-const int default_humid = 62;
+// We don´t put an component on the module at first
+const default_temp = 19;
+const default_humid = 62;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
